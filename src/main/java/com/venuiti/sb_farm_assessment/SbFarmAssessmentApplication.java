e283cbedb1c5591a1fc50e210ac13068dbff3775
Ramanuja <ramanuja29@gmail.com>
2024-04-20 20:45:12 +0530

    Initial Setup

---
+++ src/main/java/com/venuiti/sb_farm_assessment/SbFarmAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.venuiti.sb_farm_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFarmAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFarmAssessmentApplication.class, args);
+	}
+
+}
